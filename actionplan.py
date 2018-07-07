565d66837a01c3ef823b0d1ba1740224fb17e46e
bhargavi10997 <40227753+bhargavi10997@users.noreply.github.com>
2018-07-07 17:24:22 +0530

    Add files via upload

---
+++ actionplan.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class Ationplan(db.Model):
+    pid = db.relationship('Post', backref='projects', lazy=True)
+    activities = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(250), nullable=False)
+    datetime=datetime.now()
+    progress= db.Column(db.String(60), nullable=False)
+    version = db.Column(db.integer(60), nullable=False)
+
+    def __repr__(self):
+        return f"Ationplan('{self.activities}', '{self.description}', '{self.progress}','{self.version}')"
+		
+if __name__=='_main_':
+   app.run(debug=True)
