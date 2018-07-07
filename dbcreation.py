1a1c9faebb02f82c2f23140aa0582c62923b4d47
SagarikaR <32089555+SagarikaR@users.noreply.github.com>
2018-07-07 18:00:42 +0530

    final

--- dbcreation.py
+++ dbcreation.py
@@ -1,5 +1,8 @@
 from flask import *
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import *
+from datetime import *
+
+from sqlalchemy import Column, Integer, DateTime
 
 app = Flask('__main__')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
@@ -7,44 +10,55 @@ app.config['SECRET_KEY'] = ""
 
 db = SQLAlchemy(app)
 class projects(db.Model):
-   pid = db.Column(db.Integer, primary_key = True)
-   name = db.Column(db.String(100), nullable=False,unique=True)
+   pid = db.Column('projectid', db.Integer, primary_key = True)
+   name = db.Column(db.String(100),unique=True)
    locality = db.Column(db.String(50))
    address = db.Column(db.String(200))
-   date=db.Column(db.Date)
+   date= Column(DateTime)
    phone_no= db.Column(db.String(50))
    no_student=db.Column(db.Integer)
    status=db.Column(db.String(100))
 
    def __repr__(self):
-      return f"projects('{self.name}','{self.locality}','{self.address}','{self.date}','{self.phone_no}','{self.no_stude
+       return f"projects('{self.name}','{self.locality}','{self.address}','{self.date}','{self.phone_no}','{self.no_student}','{self.status}')"
+
+
 class versions(db.Model):
     version = db.Column(db.Integer, primary_key= True)
-    pid =db.column(db.Integer,db.ForeignKey('projects.pid'), nullable=False)
-    date=db.column(db.Date)
+    pid =db.column(db.Integer,db.ForeignKey('projects.pid'))
+    date= Column(DateTime)
 
     def __repr__(self):
         return f"versions('{self.date}')"
-    
+
 class templates(db.Model):
-    qid =db.Column(db.Integer,primary_key=True)
+    qid =db.Column(db.Integer, primary_key=True)
     questions=db.Column(db.String(250))
     qtype=db.Column(db.String(250))
-    
+
     def __repr__(self):
         return f"templates('{self.question}','{self.qtype}')"
-    
+
 class assessment(db.Model):
     aid=db.Column(db.Integer,primary_key=True)
-    pid=db.column(db.Integer,db.ForeignKey('projects.pid'), nullable=False)
-    qid=db.column(db.Integer,db.ForeignKey('templates.qid'), nullable=False)
+    pid=db.column(db.Integer,db.ForeignKey('projects.pid'))
+    qid=db.column(db.Integer,db.ForeignKey('templates.qid'))
     questions=db.Column(db.String(250))
-    
+
     def __repr__(self):
         return f"assessments('{self.questions}')"
+
+
+
+
+
+
 
 
 
 if __name__ == '__main__':
     app.run(debug=True)
     
+
+
+
