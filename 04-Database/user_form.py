a6021475a7ed7702bc1e7b366f7031fa9934254c
Geeta AK <geetakulkarni22@gmail.com>
2018-07-07 16:43:40 +0530

    Added user form model

---
+++ 04-Database/user_form.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class Users(db.Model):
+    Qid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(40), nullable=False)
+    role =  db.Column(db.String(30), nullable=False)
+   # posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.role}')"
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
