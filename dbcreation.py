f7cf152f6267f117bf54223c75e8422ac6b40928
SagarikaR <32089555+SagarikaR@users.noreply.github.com>
2018-07-07 18:09:06 +0530

    Update dbcreation.py

--- dbcreation.py
+++ dbcreation.py
@@ -4,7 +4,7 @@ from datetime import *
 
 from sqlalchemy import Column, Integer, DateTime
 
-app = Flask('__main__')
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SECRET_KEY'] = ""
 
