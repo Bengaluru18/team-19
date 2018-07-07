66b5431be09752e4ba2a59fc6fd641f0ff75ab82
SagarikaR <32089555+SagarikaR@users.noreply.github.com>
2018-07-07 16:35:39 +0530

    Update dbcreation.py

--- dbcreation.py
+++ dbcreation.py
@@ -17,11 +17,34 @@ class projects(db.Model):
    status=db.Column(db.String(100))
 
    def __repr__(self):
-      return f"projects('{self.name}','{self.locality}','{self.address}','{self.date}','{self.phone_no}','{self.no_student}','{self.status}')
+      return f"projects('{self.name}','{self.locality}','{self.address}','{self.date}','{self.phone_no}','{self.no_stude
+class versions(db.Model):
+    version = db.Column(db.Integer, primary_key= True)
+    pid =db.column(db.Integer,db.ForeignKey('projects.pid'), nullable=False)
+    date=db.column(db.Date)
 
+    def __repr__(self):
+        return f"versions('{self.date}')"
+    
+class templates(db.Model):
+    qid =db.Column(db.Integer,primary_key=True)
+    questions=db.Column(db.String(250))
+    qtype=db.Column(db.String(250))
+    
+    def __repr__(self):
+        return f"templates('{self.question}','{self.qtype}')"
+    
+class assessment(db.Model):
+    aid=db.Column(db.Integer,primary_key=True)
+    pid=db.column(db.Integer,db.ForeignKey('projects.pid'), nullable=False)
+    qid=db.column(db.Integer,db.ForeignKey('templates.qid'), nullable=False)
+    questions=db.Column(db.String(250))
+    
+    def __repr__(self):
+        return f"assessments('{self.questions}')"
 
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-    db.create_all()
+    
